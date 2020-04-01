3e58fd2561d93e675aaae33ec9081dfa268512ab
Sean Rich <srich366@gmail.com>
2020-04-01 15:00:37 -0400

    Run docker image tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,5 +3,6 @@ import App from "./components/App";
 describe("Main Component", () => {
   test("It exports something", () => {
     expect(App).toBeTruthy();
+    expect(App).toBeFalsy();
   });
 });
